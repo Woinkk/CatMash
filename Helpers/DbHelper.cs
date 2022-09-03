07b2464ce88fb62e407ad79dc2da5fe6421840dd
Spencer Galloni <spencer.galloni@signature-code.com>
2022-09-03 23:11:49 +0200

    Create DbHelper

---
+++ Helpers/DbHelper.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System.Data.SQLite;
+
+namespace CatMash.Helpers
+{
+    public static class DbHelper
+    {
+        public static SQLiteConnection CreateCtx( string connectionString )
+        {
+            SQLiteConnection ctx;
+            ctx = new SQLiteConnection( connectionString );
+            return ctx;
+        }
+
+        public static void CreateTable(SQLiteConnection ctx)
+        {
+            string createTable = @"
+                CREATE TABLE CatTable(
+                    Id TEXT, 
+                    Url TEXT, 
+                    Score INTEGER
+                );";
+
+            ctx.Execute(createTable);
+        }
+
+        public static void InsertData(SQLiteConnection ctx, string id, string url)
+        {
+            ctx.Execute(@"
+                INSERT INTO CatTable (Id, Url, Score) 
+                VALUES( @Id, @Url, 0 );",
+                new { Id = id, Url = url }
+            );
+        }
+    }
+}
