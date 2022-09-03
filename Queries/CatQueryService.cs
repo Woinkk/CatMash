7dd3c4350e04eb359522193d990b3a6f027a4df3
Spencer Galloni <spencer.galloni@signature-code.com>
2022-09-03 23:17:58 +0200

    Create CatQueryService
    Add query to get all cats
    Add query to update cat score

---
+++ Queries/CatQueryService.cs
@@ -0,0 +1,29 @@
+﻿using CatMash.Models;
+using Dapper;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace CatMash.Queries
+{
+    public class CatQueryService : ICatQueryService
+    {
+        public async Task<IEnumerable<Cat>> GetAllCatsAsync( SQLiteConnection ctx )
+        {
+            return await ctx.QueryAsync<Cat>(@"SELECT * FROM CatTable;");
+        }
+
+        public async Task UpdateCatScoreAsync(SQLiteConnection ctx, string id)
+        {
+            int score = await ctx.QueryFirstAsync<int>(@"SELECT Score FROM CatTable WHERE Id = @Id;", new { Id = id });
+
+            score++;
+
+            await ctx.ExecuteAsync(@"
+                UPDATE CatTable
+                SET Score = @Score
+                WHERE Id = @Id;
+            ", new { Id = id, Score = score } );
+        }
+    }
+}
---
+++ Queries/Models/ICatQueryService.cs
@@ -0,0 +1,14 @@
+﻿using CatMash.Models;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace CatMash.Queries
+{
+    public interface ICatQueryService
+    {
+        public Task<IEnumerable<Cat>> GetAllCatsAsync( SQLiteConnection ctx );
+
+        public Task UpdateCatScoreAsync( SQLiteConnection ctx, string id );
+    }
+}
