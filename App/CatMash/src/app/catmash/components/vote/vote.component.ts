da6f87c1fddc94a668c662ff545a5d3c55da16b0
Spencer Galloni <spencer.galloni@signature-code.com>
2022-09-18 16:07:13 +0200

    Add logic in vote component to set random cats after vote

--- App/CatMash/src/app/catmash/components/vote/vote.component.ts
+++ App/CatMash/src/app/catmash/components/vote/vote.component.ts
@@ -1,18 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CatmashApiService } from '../../shared/api/catmash-api.service';
+import { Cat } from '../../shared/models/cat.model';
 
 @Component({
   templateUrl: './vote.component.html',
   styleUrls: ['./vote.component.less']
 })
-export class VoteComponent implements OnInit {
+export class VoteComponent implements OnInit, OnDestroy {
+
+  private _subscription: Subscription;
+  public cats: Cat[] = [];
+  public leftCat?: Cat = undefined;
+  public rightCat?: Cat = undefined;
 
   constructor(
     private _apiService: CatmashApiService
-  ) { }
-
-  ngOnInit(): void {
-    console.log(this._apiService.getCatList());
+  ) { 
+    this._subscription = new Subscription();
   }
 
+  ngOnInit(): void {
+    this._apiService.getAllCats().subscribe(cats => {
+      this.cats = cats;
+      this.setRandomCats();
+    })
+  }
+
+  getRandomCat(): Cat | undefined {
+    if( this.cats.length === 0 ) return undefined;
+    return this.cats[Math.floor(Math.random() * this.cats.length)];
+  }
+
+  setRandomCats(): void {
+    this.leftCat = this.getRandomCat();
+    this.rightCat = this.getRandomCat();
+
+    while( this.rightCat!.id === this.leftCat!.id ){
+      this.rightCat = this.getRandomCat();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._subscription.unsubscribe();
+  }
 }
