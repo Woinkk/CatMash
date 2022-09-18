2a4978d33cc11e5071206be37e4b089e86f949db
Spencer Galloni <spencer.galloni@signature-code.com>
2022-09-18 19:11:39 +0200

    Update catmash module + add leaderboard route

--- App/CatMash/src/app/catmash/catmash-routing.module.ts
+++ App/CatMash/src/app/catmash/catmash-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '../ui/pages/layout/layout.component';
 import { VoteComponent } from './components/vote/vote.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
       {
         path: 'vote',
         component: VoteComponent
+      },
+      {
+        path: 'leaderboard',
+        component: LeaderboardComponent
       }
     ]
   }
--- App/CatMash/src/app/catmash/catmash.module.ts
+++ App/CatMash/src/app/catmash/catmash.module.ts
@@ -4,15 +4,23 @@ import { CommonModule } from '@angular/common';
 import { CatmashRoutingModule } from './catmash-routing.module';
 import { UiModule } from '../ui/ui.module';
 import { VoteComponent } from './components/vote/vote.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { CatCardComponent } from './components/cat-card/cat-card.component';
 
 
 @NgModule({
   declarations: [
-    VoteComponent
+    VoteComponent,
+    LeaderboardComponent,
+    CatCardComponent
   ],
   imports: [
     CommonModule,
     UiModule,
+    NzCardModule,
+    NzButtonModule,
     CatmashRoutingModule,
   ]
 })
